05d54e3c95a2f2462055a80cb12696475f6d1309
shinziet-gh <chenshinzie@gmail.com>
2026-04-09 16:31:43 +1200

    lab exercise 02-02 use static variable

--- lab-002/store/ShopApp.java
+++ lab-002/store/ShopApp.java
@@ -2,13 +2,13 @@ package store;
 
 public class ShopApp {
 
+    //Set tax to 20% rate
+    static double taxRate = 0.2;
+
     public static void main(String[] args) {
         //Declare two variables to reference two new Clothing objects
         Clothing item1 = new Clothing("Blue Jacket", 20.9, "M", Clothing.CONDITION.DAMAGED);
         Clothing item2 = new Clothing("Orange T-Shirt", 10.5, "S", Clothing.CONDITION.USED);
-
-        //Set tax at 20% rate
-        double taxRate = 0.2;
 
         //Create customer
         Customer cust = new Customer("Jayden");
